1fc60980c732957263372b62f0e2efd9bf317cd4
Anastasia Chernavskaia <anaschern@gmail.com>
2025-06-22 22:56:50 +0200

    Add visualizations

---
+++ marl_library/__init__.py
@@ -0,0 +1,13 @@
+from .visualization import (
+    create_training_history_gif,
+    create_training_dashboard_gif
+)
+
+__version__ = "1.0.0"
+__author__ = "Anastasia Chernavskaia: MARL Library"
+
+__all__ = [
+    # Visualization functions
+    "create_training_history_gif",
+    "create_training_dashboard_gif"
+]
